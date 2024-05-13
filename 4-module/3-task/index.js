51e20a18114e724d6df4c6bf3d23803015c17990
Albina <binnnnna@mail.ru>
2024-05-13 19:48:12 +0400

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  let row = table.tBodies[0];
+
+  for (let i = 0; i < row.rows.length; i++) {
+    if (row.rows[i].cells[3].dataset.available === "true") {
+      row.rows[i].classList.add("available");
+    } else {
+      row.rows[i].classList.add("unavailable");
+    }
+
+    if (!row.rows[i].cells[3].hasAttribute("data-available")) {
+      row.rows[i].setAttribute("hidden", "");
+    }
+
+    if (row.rows[i].cells[2].textContent === "m") {
+      row.rows[i].classList.add("male");
+    } 
+    if (row.rows[i].cells[2].textContent === "f") {
+      row.rows[i].classList.add("female");
+    }
+
+    if (parseInt(row.rows[i].cells[1].textContent) < 18) {
+      row.rows[i].style.textDecoration = "line-through";
+    }
+  }
 }
